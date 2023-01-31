617319eb51d3e609299bb2b3591507952da74ef7
Karla-Silva <mkarla@outlook.com>
2023-01-31 19:11:05 -0300

    Initial commit

---
+++ src/main/java/com/projetojava/conversaodemoedas/ConversaodemoedasApplication.java
@@ -0,0 +1,13 @@
+package com.projetojava.conversaodemoedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversaodemoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversaodemoedasApplication.class, args);
+	}
+
+}

513eded9c84c0bd0da7edd70e60253329592feff
Karla-Silva <mkarla@outlook.com>
2023-02-01 11:00:04 -0300

    api finalizada

---
+++ src/main/java/com/projetojava/conversaodemoedas/cotacaodamoeda/cotacaoDaMoedaController.java
@@ -0,0 +1,24 @@
+package com.projetojava.conversaodemoedas.cotacaodamoeda;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cotacao")
+@RequiredArgsConstructor
+public class cotacaoDaMoedaController {
+    private final cotacaoDeMoedasService cotacaoDeMoedasService;
+
+    @GetMapping("/eur")
+    public ResponseEntity<cotacaoDaMoeda[]> cotacaoEurAtual() {
+        return ResponseEntity.ok(cotacaoDeMoedasService.getCotacaoEUR());
+    }
+
+    @GetMapping("/usd")
+    public ResponseEntity<cotacaoDaMoeda[]> cotacaoUsdAtual() {
+        return ResponseEntity.ok(cotacaoDeMoedasService.getCotacaoUSD());
+    }
+}
